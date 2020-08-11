884181513211b898fba7b096ee2177a04ee8c701
linsanityHuang <js_huang2018@163.com>
2020-08-12 00:21:39 +0800

    add comment

--- chap-20/select01.c
+++ chap-20/select01.c
@@ -1,10 +1,28 @@
 #include "lib/common.h"
 
-#define  MAXLINE     1024
+#define MAXLINE 1024
+/*
+我一直很好奇，为啥说select函数对fd有1024的限制，找了点资料共勉：
+首先，man select，搜索FD_SETSIZE会看到如下的内容
+An fd_set is a fixed size buffer. 
+Executing FD_CLR() or FD_SET() with a value of fd 
+that is negative or is equal to or larger than FD_SETSIZE will result in undefined behavior. 
+Moreover, POSIX requires fd to be a valid file descriptor.
+其中最关键的是FD_SETSIZE，是在bitmap位图运算的时候会受到他的影响
+其次，sys/select.h头文件有如下定义：
+#define FD_SETSIZE __FD_SETSIZE
+typesizes.h头文件有如下定义：
+#define __FD_SETSIZE 1024
 
+由此，终于看到了1024的准确限制。
 
-int main(int argc, char **argv) {
-    if (argc != 2) {
+同时man里也说明了一个限制，不是0-1023的fd会导致未定义的行为。
+*/
+
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
         error(1, 0, "usage: select01 <IPaddress>");
     }
     int socket_fd = tcp_client(argv[1], SERV_PORT);
@@ -14,23 +32,49 @@ int main(int argc, char **argv) {
 
     fd_set readmask;
     fd_set allreads;
+
+    // FD_ZERO 用来将这个向量的所有元素都设置成 0
+    // allreads可以看作是一个数组, 每个元素只能为0, 1, 即位图
     FD_ZERO(&allreads);
+
+    // FD_SET 用来把对应套接字 fd 的元素，a[fd]设置成 1
+    // 把标准输入0和连接套接字3设置为1
     FD_SET(0, &allreads);
     FD_SET(socket_fd, &allreads);
 
-    for (;;) {
+    for (;;)
+    {
+        // 注意
+        // 每次测试完之后，重新设置待测试的描述符集合
+        // 因为 select 调用每次完成测试之后，内核都会修改描述符集合，
+        // 通过修改完的描述符集合来和应用程序交互，
+        // 应用程序使用 FD_ISSET 来对每个描述符进行判断，从而知道什么样的事件发生
         readmask = allreads;
+        // int select(int maxfd, fd_set *readset, fd_set *writeset, fd_set *exceptset, const struct timeval *timeout);
+        // 返回：若有就绪描述符则为其数目，若超时则为0，若出错则为 - 1
+        // timeout为NULL, 表示如果没有 I/O 事件发生，则 select 一直等待下去
+        // 使用 socket_fd+1 来表示待测试的描述符基数。切记需要 +1
         int rc = select(socket_fd + 1, &readmask, NULL, NULL, NULL);
 
-        if (rc <= 0) {
+        if (rc <= 0)
+        {
             error(1, errno, "select failed");
         }
 
-        if (FD_ISSET(socket_fd, &readmask)) {
+        // FD_ISSET 对这个向量进行检测，判断出对应套接字的元素 a[fd]是 0 还是 1
+        // 其中 0 代表不需要处理，1 代表需要处理
+
+        // 如果是连接描述字准备好可读了,
+        // 使用 read 将套接字数据读出
+        if (FD_ISSET(socket_fd, &readmask))
+        {
             n = read(socket_fd, recv_line, MAXLINE);
-            if (n < 0) {
+            if (n < 0)
+            {
                 error(1, errno, "read error");
-            } else if (n == 0) {
+            }
+            else if (n == 0)
+            {
                 error(1, 0, "server terminated \n");
             }
             recv_line[n] = 0;
@@ -38,23 +82,25 @@ int main(int argc, char **argv) {
             fputs("\n", stdout);
         }
 
-        if (FD_ISSET(STDIN_FILENO, &readmask)) {
-            if (fgets(send_line, MAXLINE, stdin) != NULL) {
+        // 标准输入0可读, 程序读入后发送给对端
+        if (FD_ISSET(STDIN_FILENO, &readmask))
+        {
+            if (fgets(send_line, MAXLINE, stdin) != NULL)
+            {
                 int i = strlen(send_line);
-                if (send_line[i - 1] == '\n') {
+                if (send_line[i - 1] == '\n')
+                {
                     send_line[i - 1] = 0;
                 }
 
                 printf("now sending %s\n", send_line);
                 ssize_t rt = write(socket_fd, send_line, strlen(send_line));
-                if (rt < 0) {
+                if (rt < 0)
+                {
                     error(1, errno, "write failed ");
                 }
                 printf("send bytes: %zu \n", rt);
             }
         }
     }
-
 }
-
-
--- chap-21/pollserver.c
+++ chap-21/pollserver.c
@@ -2,68 +2,139 @@
 
 #define INIT_SIZE 128
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     int listen_fd, connected_fd;
     int ready_number;
     ssize_t n;
     char buf[MAXLINE];
     struct sockaddr_in client_addr;
 
+    // 创建一个监听套接字，并绑定在本地的地址和端口上
     listen_fd = tcp_server_listen(SERV_PORT);
 
-    //初始化pollfd数组，这个数组的第一个元素是listen_fd，其余的用来记录将要连接的connect_fd
+    // struct pollfd
+    // {
+    //     int fd;          /* file descriptor */
+    //     short events;    /* events to look for */
+    //     short revents;   /* events returned */
+    // };
+
+    // 可读事件
+    // #define POLLIN 0x0001     /* any readable data available */
+    // #define POLLPRI 0x0002    /* OOB/Urgent readable data */
+    // #define POLLRDNORM 0x0040 /* non-OOB/URG data available */
+    // #define POLLRDBAND 0x0080 /* OOB/Urgent readable data */
+
+    // 可写事件
+    // #define POLLOUT 0x0004     /* file descriptor is writeable */
+    // #define POLLWRNORM POLLOUT /* no write type differentiation */
+    // #define POLLWRBAND 0x0100  /* OOB/Urgent data can be written */
+
+    // 错误事件
+    // #define POLLERR 0x0008  /* 一些错误发送 */
+    // #define POLLHUP 0x0010  /* 描述符挂起*/
+    // #define POLLNVAL 0x0020 /* 请求的事件无效*/
+
+    // 初始化pollfd数组，这个数组的第一个元素是listen_fd，其余的用来记录将要连接的connect_fd
+    // 这里数组的大小固定为 INIT_SIZE，这在实际的生产环境肯定是需要改进的。
     struct pollfd event_set[INIT_SIZE];
+
+    // 将监听套接字 listen_fd 和对应的 POLLRDNORM 事件加入到 event_set 里，
+    // 表示我们期望系统内核检测监听套接字上的连接建立完成事件
     event_set[0].fd = listen_fd;
     event_set[0].events = POLLRDNORM;
 
     // 用-1表示这个数组位置还没有被占用
+    // 将 event_set 数组里其他没有用到的 fd 统统设置为 -1。
+    // 这里 -1 也表示了当前 pollfd 没有被使用的意思
+    // poll 函数将会忽略这个 pollfd
     int i;
-    for (i = 1; i < INIT_SIZE; i++) {
+    for (i = 1; i < INIT_SIZE; i++)
+    {
         event_set[i].fd = -1;
     }
 
-    for (;;) {
-        if ((ready_number = poll(event_set, INIT_SIZE, -1)) < 0) {
+    for (;;)
+    {
+        // int poll(struct pollfd *fds, unsigned long nfds, int timeout);
+        // 返回值：若有就绪描述符则为其数目，若超时则为0，若出错则为-1
+        if ((ready_number = poll(event_set, INIT_SIZE, -1)) < 0)
+        {
             error(1, errno, "poll failed ");
         }
 
-        if (event_set[0].revents & POLLRDNORM) {
+        // 系统内核检测到监听套接字上的连接建立事件
+        if (event_set[0].revents & POLLRDNORM)
+        {
             socklen_t client_len = sizeof(client_addr);
-            connected_fd = accept(listen_fd, (struct sockaddr *) &client_addr, &client_len);
+
+            // 调用 accept 函数获取连接描述字
+            connected_fd = accept(listen_fd, (struct sockaddr *)&client_addr, &client_len);
 
             //找到一个可以记录该连接套接字的位置
-            for (i = 1; i < INIT_SIZE; i++) {
-                if (event_set[i].fd < 0) {
+            for (i = 1; i < INIT_SIZE; i++)
+            {
+                if (event_set[i].fd < 0)
+                {
+                    // 把连接描述字 connect_fd 也加入到 event_set 里
+                    // 而且说明了我们感兴趣的事件类型为 POLLRDNORM
+                    // 也就是套接字上有数据可以读
                     event_set[i].fd = connected_fd;
                     event_set[i].events = POLLRDNORM;
                     break;
                 }
             }
 
-            if (i == INIT_SIZE) {
+            if (i == INIT_SIZE)
+            {
                 error(1, errno, "can not hold so many clients");
             }
 
+            // 一个加速优化能力
+            // 因为 poll 返回的一个整数, 说明了这次 I/O 事件描述符的个数,
+            // 如果处理完监听套接字之后，就已经完成了这次 I/O 复用所要处理的事情,
+            // 即ready_number = 1
+            // 那么我们就可以跳过后面的处理(连接套接字的读写事件)，再次进入 poll 调用
+            // 因为这个分支是监听套接字的可读事件处理逻辑且只有一个事件, 那说明只有监听套接字一个事件发生
             if (--ready_number <= 0)
                 continue;
         }
 
-        for (i = 1; i < INIT_SIZE; i++) {
+        // 接下来的循环处理是查看 event_set 里面其他的事件,
+        // 也就是已连接套接字的可读事件
+        // 这是通过遍历 event_set 数组来完成的
+        for (i = 1; i < INIT_SIZE; i++)
+        {
             int socket_fd;
+            // 如果数组里的 pollfd 的 fd 为 -1，说明这个 pollfd 没有递交有效的检测，直接跳过
             if ((socket_fd = event_set[i].fd) < 0)
                 continue;
-            if (event_set[i].revents & (POLLRDNORM | POLLERR)) {
-                if ((n = read(socket_fd, buf, MAXLINE)) > 0) {
-                    if (write(socket_fd, buf, n) < 0) {
+
+            // 通过检测 revents 的事件类型是 POLLRDNORM 或者 POLLERR，我们可以进行读操作
+            if (event_set[i].revents & (POLLRDNORM | POLLERR))
+            {
+                if ((n = read(socket_fd, buf, MAXLINE)) > 0)
+                {
+                    if (write(socket_fd, buf, n) < 0)
+                    {
                         error(1, errno, "write error");
                     }
-                } else if (n == 0 || errno == ECONNRESET) {
+                }
+                else if (n == 0 || errno == ECONNRESET)
+                {
                     close(socket_fd);
                     event_set[i].fd = -1;
-                } else {
+                }
+                else
+                {
                     error(1, errno, "read error");
                 }
 
+                // 和前面的优化加速处理一样
+                // 判断如果事件已经被完全处理完之后
+                // 直接跳过对 event_set 的循环处理
+                // 再次来到 poll 调用
                 if (--ready_number <= 0)
                     break;
             }
